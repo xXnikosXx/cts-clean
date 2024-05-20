853b8fe9b09156c7e78075811a4473f2b52d2516
Nikolaos Motos <nmotos@ctsolutions.gr>
2024-05-20 22:11:57 +0300

    Added Strapi as backend with intent to add Internationalization. Also added (and tinkered a bit with) i18next.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { i18n } = require("./i18next.config");
 
-export default nextConfig;
+const nextConfig = {
+    i18n,
+    // reactStrictMode: true,
+}
+
+module.exports = nextConfig;
